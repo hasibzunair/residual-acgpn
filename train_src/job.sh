7c74342757faeda704a43d8ce8149739e0dd6be5
hasibzunair <hasibzunair@gmail.com>
2021-11-23 17:42:50 -0500

    job working with 2 gpus

--- train_src/job.sh
+++ train_src/job.sh
@@ -5,15 +5,15 @@
 
 # Specify nodes
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100:8 # 8
-#SBATCH --cpus-per-task=24 # 24
+#SBATCH --gres=gpu:p100:2 # 8, v100
+#SBATCH --cpus-per-task=6 # 24
 #SBATCH --mem=16G
 
 # Specify time
 # time (DD-HH:MM)
 
 # Run for a day
-#SBATCH --time=5-00:00 
+#SBATCH --time=3-00:00 
 
 # NOT USING THIS
 # Run less than a day
@@ -42,14 +42,15 @@ pip install tensorboardX
 # source /home/hasib/projects/def-abhamza/hasib/ENVS/tryon/bin/activate
 
 # Load CUDA and see GPUs
-#module load arch/avx512 StdEnv/2018.3
+# module load arch/avx512 StdEnv/2018.3
 module load cuda cudnn
 nvidia-smi
 
 # Run test script
-python ./train.py  --name resunet_g1 --gpu_ids 0,1,2,3,4,5,6,7 --batchSize 8
+python ./train.py  --name resunet_g1 --gpu_ids 0,1 --batchSize 4
+# python ./train.py  --name resunet_g1 --gpu_ids 0,1,2,3 --batchSize 8
 
 # See GPU usage in job
 # sq -i 5
-# srun --jobid 123456 --pty watch -n 30 nvidia-smi
+# srun --jobid 123456 --pty watch -n 2 nvidia-smi
 # https://github.com/hasibzunair/residual-acgpn/tree/graham
